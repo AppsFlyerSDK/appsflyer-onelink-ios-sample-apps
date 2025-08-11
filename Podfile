d5e013146cd78e75cc2b6b922dfaf12a4be29bf5
Amit Levy <amit.levy@appsflyer.com>
2025-08-11 12:19:03 +0300

    Working Branch migration

--- Podfile
+++ Podfile
@@ -5,7 +5,8 @@ target 'basic_app' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-#pod 'AppsFlyerFramework','6.14.3'
+pod 'AppsFlyerFramework','6.17.2'
+pod 'AppsFlyerMigrationHelper'
 
 # For Branch iOS SDK 2.0.0+
 pod 'BranchSDK'
--- basic_app/AppDelegate.swift
+++ basic_app/AppDelegate.swift
@@ -1,75 +1,120 @@
+//
+//  AppDelegate.swift
+//  basic_app
+//
+//  Created by Liaz Kamper on 11/05/2020.
+//  Copyright © 2020 OneLink. All rights reserved.
+//
+
 import UIKit
 import AppsFlyerLib
 import AppTrackingTransparency
-import BranchSDK
 import AppsFlyerMigrationHelper
+import BranchSDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var ConversionData: [AnyHashable: Any]? = nil
     var window: UIWindow?
-    var deferred_deep_link_processed_flag: Bool = false
+    var deferred_deep_link_processed_flag:Bool = false
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        configureAppsFlyer()
-        configureBranch(with: launchOptions)
-        AppsFlyerLib.shared().start()
-        return true
-    }
-    
-    private func configureBranch(with launchOptions: [UIApplication.LaunchOptionsKey: Any]?) {
+        
+        let isBranchSDKAvailable = NSClassFromString("Branch") != nil
+
         Branch.enableLogging()
-        handlePasteboardOnInstall()
         
-        Branch.getInstance().initSession(launchOptions: launchOptions) { (params, error) in
-            NSLog("[Branch] initSession, deep link data: %@", params ?? [:])
-            if let params = params as? [String: AnyObject] {
-                AppsFlyerMigrationHelper.shared.setDeepLinkingData(params)
+        if #available(iOS 16.0, *) {
+                // Don't check pasteboard on install, instead utilize UIPasteControl
+            } else if #available(iOS 15.0, *) {
+                // Call `checkPasteboardOnInstall()` before Branch initialization
+                Branch.getInstance().checkPasteboardOnInstall()
             }
-        }
-        
-        if let sessionParams = Branch.getInstance().getLatestReferringParams() {
-            AppsFlyerMigrationHelper.shared.setDeepLinkingData(sessionParams)
-        }
-        
-        Branch.getInstance().lastAttributedTouchData(withAttributionWindow: 0) { (params, error) in
-            if let params = params {
-                AppsFlyerMigrationHelper.shared.setAttributionData(params.lastAttributedTouchJSON)
-            }
-        }
-    }
-    
-    private func configureAppsFlyer() {
+
+            // Check if pasteboard toast will show
+            if Branch.getInstance().willShowPasteboardToast(){
+                // You can notify the user of what just occurred here
+                NSLog("[Branch] willShowPasteboardToast ######")
+          }
         AppsFlyerLib.shared().appleAppID = "1201211633"
         AppsFlyerLib.shared().appsFlyerDevKey = "WdpTVAcYwmxsaQ4WeTspmh"
         AppsFlyerLib.shared().isDebug = true
-    }
-    
-    private func handlePasteboardOnInstall() {
-        if #available(iOS 16.0, *) {
-            return
-        } else if #available(iOS 15.0, *) {
-            Branch.getInstance().checkPasteboardOnInstall()
+        
+        let isFirstSession = !UserDefaults.standard.bool(forKey: "isFirstSession")
+        
+        if isFirstSession {
+            UserDefaults.standard.set(true, forKey: "isFirstSession")
+            UserDefaults.standard.synchronize()
         }
         
-        if Branch.getInstance().willShowPasteboardToast() {
-            NSLog("[Branch] willShowPasteboardToast")
+        Branch.getInstance().initSession(launchOptions: launchOptions) { (universalObject, linkProperties, error) in
+            // Access and use deep link data here (nav to page, display content, etc.)
+            if !isFirstSession, let universalObject = universalObject {
+                AFMigrationHelper.shared.setDeepLinkingData(Branch.getInstance().getLatestReferringParams())
+            }
+                    
+            if isFirstSession {
+                let dispatchGroup = DispatchGroup()
+                dispatchGroup.enter()
+                
+                DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 3) {
+                    Branch.getInstance().lastAttributedTouchData(withAttributionWindow:0) { (params, error) in
+                        if let params = params {
+                            AFMigrationHelper.shared.setAttributionData(params.lastAttributedTouchJSON, attributionWindow: params.attributionWindow)
+                            AppsFlyerLib.shared().start()
+                        }
+                    }
+                    dispatchGroup.leave()
+                }
+            } else {
+                AppsFlyerLib.shared().start()
+            }
         }
+        return true
     }
+
+    // Open Universal Links
     
+    // For Swift version < 4.2 replace function signature with the commented out code
+    // func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool { // this line for Swift < 4.2
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        return Branch.getInstance().continue(userActivity)
+        
+        // Handler for Universal Links
+        Branch.getInstance().continue(userActivity)
+        return true
+    }
+            
+    // Open URI-scheme for iOS 9 and above
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return true
     }
     
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        return Branch.getInstance().application(app, open: url, options: options)
+       
+    // User logic
+    fileprivate func walkToSceneWithParams(fruitName: String, deepLinkData: [String: Any]?) {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        UIApplication.shared.windows.first?.rootViewController?.dismiss(animated: true, completion: nil)
+               
+        let destVC = fruitName + "_vc"
+        if let newVC = storyBoard.instantiateVC(withIdentifier: destVC) {
+            
+            NSLog("[AFSDK] AppsFlyer routing to section: \(destVC)")
+            newVC.deepLinkData = deepLinkData
+            
+             UIApplication.shared.windows.first?.rootViewController?.present(newVC, animated: true, completion: nil)
+        } else {
+            NSLog("[AFSDK] AppsFlyer: could not find section: \(destVC)")
+        }
     }
 }
 
 extension UIStoryboard {
     func instantiateVC(withIdentifier identifier: String) -> DLViewController? {
-        if let identifiersList = self.value(forKey: "identifierToNibNameMap") as? [String: Any], identifiersList[identifier] != nil {
-            return self.instantiateViewController(withIdentifier: identifier) as? DLViewController
+        // "identifierToNibNameMap" – dont change it. It is a key for searching IDs
+        if let identifiersList = self.value(forKey: "identifierToNibNameMap") as? [String: Any] {
+            if identifiersList[identifier] != nil {
+                return self.instantiateViewController(withIdentifier: identifier) as? DLViewController
+            }
         }
         return nil
     }
